3557f1011d8ecae6e204d83a58264feade1a56fd
hkimca <harrison.kim.ca@gmail.com>
2017-07-02 13:17:26 -0400

    Initial Commit

---
+++ w1d5_pizzaRestaurant_delegates/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  w1d5_pizzaRestaurant_delegates
+//
+//  Created by Seantastic31 on 02/07/2017.
+//  Copyright © 2017 Seantastic31. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
